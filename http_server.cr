800c924e3a6d184a0b8fb0fc7b2d6313e74b40cf
wellflat <wellflat@gmail.com>
2019-05-24 00:26:21 +0900

    fixed HTTP::Server.new spec

--- http_server.cr
+++ http_server.cr
@@ -2,10 +2,11 @@
 
 require "http/server"
 
-server = HTTP::Server.new(8080) do |context|
+server = HTTP::Server.new do |context|
   context.response.content_type = "text/plain"
   context.response.print "Hello world! The time is #{Time.now}"
 end
 
-puts "Listening on http://127.0.0.1:8080"
+address = server.bind_tcp 8080
+puts "Listening on http://#{address}"
 server.listen
